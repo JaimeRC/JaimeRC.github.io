9e06184a3e321e3e9a81ce0f4ca364842e832041
JaimeRC <jaime.rc@icloud.com>
2021-03-17 22:27:14 +0100

    commit initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './theme';
+import AppAppBar from "./view/AppAppBar";
+import ProductHero from "./view/ProductHero";
+import ProductValues from "./view/ProductValues";
+import ProductCategories from "./view/ProductCategories";
+import ProductHowItWorks from "./view/ProductHowItWorks";
+import ProductCTA from "./view/ProductCTA";
+import ProductSmokingHero from "./view/ProductSmokingHero";
+import AppFooter from "./view/AppFooter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const App = () => (
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+        <AppAppBar />
+        <ProductHero />
+        <ProductValues />
+        <ProductCategories />
+        <ProductHowItWorks />
+        <ProductCTA />
+        <ProductSmokingHero />
+        <AppFooter />
+    </ThemeProvider>
+)
+export default App
