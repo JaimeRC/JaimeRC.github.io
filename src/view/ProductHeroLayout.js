57b915222b1af07428b12c9ccc01ba6bfc87523c
JaimeRC <jaime.rc@icloud.com>
2021-03-17 22:26:16 +0100

    add views

--- src/view/ProductHeroLayout.js
+++ src/view/ProductHeroLayout.js
@@ -52,19 +52,21 @@ const styles = (theme) => ({
 
 function ProductHeroLayout(props) {
     const { backgroundClassName, children, classes } = props;
-
     return (
         <section className={classes.root}>
             <Container className={classes.container}>
+                {/*
                 <img
                     src="/static/themes/onepirate/productHeroWonder.png"
                     alt="wonder"
                     width="147"
                     height="80"
                 />
+                */}
                 {children}
                 <div className={classes.backdrop} />
                 <div className={clsx(classes.background, backgroundClassName)} />
+                {/*
                 <img
                     className={classes.arrowDown}
                     src="/static/themes/onepirate/productHeroArrowDown.png"
@@ -72,6 +74,7 @@ function ProductHeroLayout(props) {
                     width="12"
                     alt="arrow down"
                 />
+                                */}
             </Container>
         </section>
     );
