9864772cf11e93c5c51cc5a1f8209a9d9d757220
JaimeRC <jaime.rc@icloud.com>
2021-04-06 19:31:50 +0200

    feat: update Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,20 @@
 import {faGithub, faStackOverflow, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-export default function (props) {
+function Navbar(props) {
     return (
         <div>
-            <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-dark">
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <span className="navbar-text text-light">{"Jaime Rubio"}</span>
-                    <div className="d-flex">
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
+                            aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <a className="navbar-brand" href="#">{"Jaime Rubio"}</a>
+                    <div className="collapse navbar-collapse" id="navbarToggler">
+                        <div className="navbar-nav me-auto"/>
+                        <form className="d-flex">
+                            <ul className="navbar-nav me-2">
                                 <li className="nav-item">
                                     <a
                                         rel="noreferrer"
@@ -36,11 +41,11 @@ export default function (props) {
                                     </a>
                                 </li>
                             </ul>
-                        </div>
+                        </form>
                     </div>
-
                 </div>
             </nav>
         </div>)
 }
 
+export default Navbar
