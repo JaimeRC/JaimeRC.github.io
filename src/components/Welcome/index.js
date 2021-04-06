9dd232b8616ee0d9faabfef981b484fbd0aa9616
JaimeRC <jaime.rc@icloud.com>
2021-04-06 19:31:11 +0200

    feat: update Welcome

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,16 +1,17 @@
-import {personal} from "../../data/profile";
 import './styles.css'
 
-export default function () {
+function Welcome() {
     return (
-            <div className={"row mt-5 home-image"}>
-                <div className={"col-sm-1 col-md-1 col-lg-2 col-xl-4"}/>
-                <div className={"col-sm-10 col-md-10 col-lg-6 col-xl-4 text-white"}>
-                    <h1 className={"fw-bold text-center"}>{"Welcome"}</h1>
-                    <p className="lead fw-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example
-                        based on Apple’s marketing pages.</p>
-                </div>
-                <div className={"col-sm-1 col-md-1 col-lg-2 col-xl-4"}/>
+        <div className={"row mt-5 home-image"}>
+            <div className={"col-sm-1 col-md-1 col-lg-2 col-xl-4"}/>
+            <div className={"col-sm-10 col-md-10 col-lg-6 col-xl-4 text-white align-self-center"}>
+                <h1 className={"fw-bold text-center"}>{"Welcome"}</h1>
+                <p className={"lead fw-normal text-center"}>And an even wittier subheading to boot. Jumpstart your marketing efforts with this example
+                    based on Apple’s marketing pages.</p>
             </div>
+            <div className={"col-sm-1 col-md-1 col-lg-2 col-xl-4"}/>
+        </div>
     )
 }
+
+export default  Welcome
